fa0590bcb9c1c1d945408aeed1b61341735ba87e
Clay <clay@clay66.dev>
2024-10-02 10:19:20 +0100

    Initial visual interface design
    No interactivity yet just dumbass visuals

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
 
+#include "slint.h"
 #include "utils.hpp"
+#include "window.h"
 
 int main() {
-    std::cout << "Hello there!\n";
-    std::cout << storage_dir() << '\n';
+    std::cout << "Storing at " << storage_dir() << '\n';
+    auto main_window = MainWindow::create();
+    main_window->run();
 }
