ebc50643ea35cdd1edb656e916f96f4c554340fb
Clay <clay@clay66.dev>
2024-10-14 10:34:58 +0100

    Update the storage dir

--- src/backend.cpp
+++ src/backend.cpp
@@ -4,8 +4,10 @@ std::string storage_dir() {
     std::string root_dir;
 #ifdef _WIN32
     root_dir = getenv("APPDATA");
+    root_dir += "/microtorrent";
 #elif __linux__
-    root_dir = getenv("HOME");
+    root_dir = getenv("HOME") + "/.microtorrent";
+    root_dir += "/.microtorrent"
 #endif
-    return root_dir + "/microtorrent";
+    return root_dir;
 }
--- src/backend.hpp
+++ src/backend.hpp
@@ -3,5 +3,7 @@
 #include <string>
 
 /// @brief Return the directory in which MicroTorrent will store all its data
+///
+/// ~/.microtorrent on Linux, and %USERPROFILE% on Windows
 /// @return The storage directory path as a string
 std::string storage_dir();
