e228ccd2bf5ef5336871e8de382d1b3a0bdc3df5
Clay <clay@clay66.dev>
2024-10-14 10:30:37 +0100

    Remember how CMake works

--- src/backend.hpp
+++ src/backend.hpp
@@ -4,12 +4,4 @@
 
 /// @brief Return the directory in which MicroTorrent will store all its data
 /// @return The storage directory path as a string
-std::string storage_dir() {
-    std::string root_dir;
-#ifdef _WIN32
-    root_dir = getenv("APPDATA");
-#elif __linux__
-    root_dir = getenv("HOME");
-#endif
-    return root_dir + "/microtorrent";
-}
+std::string storage_dir();
