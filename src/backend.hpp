0c51ec4d63cd84f745f257ae08c24e49ec146936
Clay <clay@clay66.dev>
2024-10-14 10:29:05 +0100

    Sure man I'll just use headers why not

--- src/backend.hpp
+++ src/backend.hpp
@@ -4,4 +4,12 @@
 
 /// @brief Return the directory in which MicroTorrent will store all its data
 /// @return The storage directory path as a string
-std::string storage_dir();
+std::string storage_dir() {
+    std::string root_dir;
+#ifdef _WIN32
+    root_dir = getenv("APPDATA");
+#elif __linux__
+    root_dir = getenv("HOME");
+#endif
+    return root_dir + "/microtorrent";
+}
