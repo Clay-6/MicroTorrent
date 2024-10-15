b49cbb6fee4b019abc5e6f9d3a93b0a3781435ce
Clay <clay@clay66.dev>
2024-10-15 12:44:41 +0100

    Create data directory if it does not exist

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,19 @@
+#include <filesystem>
 #include <iostream>
+#include <libtorrent/alert_types.hpp>
+#include <libtorrent/session.hpp>
+#include <libtorrent/session_params.hpp>
 
 #include "backend.hpp"
 #include "slint.h"
 #include "window.h"
 
 int main() {
+    if (!std::filesystem::exists(storage_dir())) {
+        std::filesystem::create_directory(storage_dir());
+    }
     std::cout << "Storing at " << storage_dir() << '\n';
+
     auto main_window = MainWindow::create();
     main_window->run();
 }
