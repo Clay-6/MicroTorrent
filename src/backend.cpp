9c3ed9ccf2b63d92d8aa50e423ab9b128fbbc5f3
Clay <clay@clay66.dev>
2024-10-15 12:26:27 +0100

    Add torrent resuming function

--- src/backend.cpp
+++ src/backend.cpp
@@ -1,5 +1,9 @@
 #include "backend.hpp"
 
+#include <filesystem>
+#include <fstream>
+#include <libtorrent/read_resume_data.hpp>
+
 std::string storage_dir() {
     std::string root_dir;
 #ifdef _WIN32
@@ -10,4 +14,21 @@ std::string storage_dir() {
     root_dir += "/.microtorrent"
 #endif
     return root_dir;
+}
+
+std::vector<lt::add_torrent_params> resume_torrents() {
+    std::vector<lt::add_torrent_params> torrents;
+    std::filesystem::directory_iterator dir(storage_dir() + "/resume-files");
+    for (const std::filesystem::directory_entry& entry : dir) {
+        std::ifstream ifs(entry.path(), std::ios_base::binary);
+        ifs.unsetf(std::ios_base::skipws);
+        std::vector<char> buf{std::istream_iterator<char>(ifs),
+                              std::istream_iterator<char>()};
+        if (buf.size() > 0) {
+            lt::add_torrent_params params = lt::read_resume_data(buf);
+            torrents.emplace_back(params);
+        }
+    }
+
+    return torrents;
 }
--- src/backend.hpp
+++ src/backend.hpp
@@ -1,9 +1,15 @@
 #pragma once
 
+#include <libtorrent/add_torrent_params.hpp>
 #include <string>
+#include <vector>
 
 /// @brief Return the directory in which MicroTorrent will store all its data
 ///
 /// ~/.microtorrent on Linux, and %USERPROFILE% on Windows
 /// @return The storage directory path as a string
 std::string storage_dir();
+
+/// @brief Resume torrents that have been saved previously
+/// @return A vector of the torrents which were resumed, this may be empty
+std::vector<lt::add_torrent_params> resume_torrents();
