4266107c1868cf34f42429db7665cb6d059d7352
Clay <clay@clay66.dev>
2024-11-26 12:37:22 +0000

    Add comments

--- src/backend.cpp
+++ src/backend.cpp
@@ -8,6 +8,8 @@
 namespace mt {
     std::string storage_dir() noexcept {
         std::string root_dir;
+        // Each platform needs its own handling to find the desired directory,
+        // so do it in a preprocessor directive so only one version is compiled
 #ifdef _WIN32
         root_dir = getenv("APPDATA");
         root_dir += "/microtorrent";
@@ -22,7 +24,7 @@ namespace mt {
 
     std::vector<char> load_file(const char *filename) {
         std::ifstream ifs(filename, std::ios_base::binary);
-        ifs.unsetf(std::ios_base::skipws);
+        ifs.unsetf(std::ios_base::skipws); // Don't skip whitespace
         return {std::istream_iterator<char>(ifs), std::istream_iterator<char>()};
     }
 
@@ -31,14 +33,18 @@ namespace mt {
         std::vector<lt::add_torrent_params> torrents;
         if (!fs::exists(resume_dir)) {
             fs::create_directory(resume_dir);
+            return {}; // Just return an empty list since the directory didn't exist anyways
         }
+
         fs::directory_iterator dir(resume_dir);
+
         for (const fs::directory_entry &entry: dir) {
             std::ifstream ifs(entry.path(), std::ios_base::binary);
             ifs.unsetf(std::ios_base::skipws);
+            // Load the file into a char buffer
             std::vector<char> buf{std::istream_iterator<char>(ifs),
                                   std::istream_iterator<char>()};
-            if (buf.size() > 0) {
+            if (!buf.empty()) {
                 lt::add_torrent_params params = lt::read_resume_data(buf);
                 torrents.emplace_back(params);
             }
