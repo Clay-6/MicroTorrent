f22baeed79aaee17677f0513277ae099dde26392
Clay <clay@clay66.dev>
2024-11-26 12:20:07 +0000

    Add namespace to backend functions

--- src/backend.cpp
+++ src/backend.cpp
@@ -5,43 +5,45 @@
 #include <libtorrent/read_resume_data.hpp>
 #include <libtorrent/write_resume_data.hpp>
 
-std::string storage_dir() noexcept {
-    std::string root_dir;
+namespace mt {
+    std::string storage_dir() noexcept {
+        std::string root_dir;
 #ifdef _WIN32
-    root_dir = getenv("APPDATA");
-    root_dir += "/microtorrent";
+        root_dir = getenv("APPDATA");
+        root_dir += "/microtorrent";
 #elif __linux__
-    root_dir = getenv("HOME") + "/.microtorrent";
-    root_dir += "/.microtorrent"
+        root_dir = getenv("HOME") + "/.microtorrent";
+        root_dir += "/.microtorrent"
 #endif
-    return root_dir;
-}
-
-namespace fs = std::filesystem;
-
-std::vector<char> load_file(const char* filename) {
-    std::ifstream ifs(filename, std::ios_base::binary);
-    ifs.unsetf(std::ios_base::skipws);
-    return {std::istream_iterator<char>(ifs), std::istream_iterator<char>()};
-}
-
-std::vector<lt::add_torrent_params> resume_torrents() noexcept {
-    std::string resume_dir = storage_dir() + "/resume-files";
-    std::vector<lt::add_torrent_params> torrents;
-    if (!fs::exists(resume_dir)) {
-        fs::create_directory(resume_dir);
+        return root_dir;
     }
-    fs::directory_iterator dir(resume_dir);
-    for (const fs::directory_entry& entry : dir) {
-        std::ifstream ifs(entry.path(), std::ios_base::binary);
+
+    namespace fs = std::filesystem;
+
+    std::vector<char> load_file(const char *filename) {
+        std::ifstream ifs(filename, std::ios_base::binary);
         ifs.unsetf(std::ios_base::skipws);
-        std::vector<char> buf{std::istream_iterator<char>(ifs),
-                              std::istream_iterator<char>()};
-        if (buf.size() > 0) {
-            lt::add_torrent_params params = lt::read_resume_data(buf);
-            torrents.emplace_back(params);
-        }
+        return {std::istream_iterator<char>(ifs), std::istream_iterator<char>()};
     }
 
-    return torrents;
-}
+    std::vector<lt::add_torrent_params> resume_torrents() noexcept {
+        std::string resume_dir = storage_dir() + "/resume-files";
+        std::vector<lt::add_torrent_params> torrents;
+        if (!fs::exists(resume_dir)) {
+            fs::create_directory(resume_dir);
+        }
+        fs::directory_iterator dir(resume_dir);
+        for (const fs::directory_entry &entry: dir) {
+            std::ifstream ifs(entry.path(), std::ios_base::binary);
+            ifs.unsetf(std::ios_base::skipws);
+            std::vector<char> buf{std::istream_iterator<char>(ifs),
+                                  std::istream_iterator<char>()};
+            if (buf.size() > 0) {
+                lt::add_torrent_params params = lt::read_resume_data(buf);
+                torrents.emplace_back(params);
+            }
+        }
+
+        return torrents;
+    }
+} // namespace mt
--- src/backend.hpp
+++ src/backend.hpp
@@ -5,18 +5,20 @@
 #include <string_view>
 #include <vector>
 
+namespace mt {
 /// @brief Return the directory in which MicroTorrent will store all its data
 ///
 /// $HOME/.microtorrent on Linux, and %USERPROFILE% on Windows
 /// @return The storage directory path as a string
-std::string storage_dir() noexcept;
+    std::string storage_dir() noexcept;
 
 /// @brief Resume torrents that have been saved previously
 /// @return A vector of the torrents which were resumed. This may be empty
 /// if no torrents were actually resumed
-std::vector<lt::add_torrent_params> resume_torrents() noexcept;
+    std::vector<lt::add_torrent_params> resume_torrents() noexcept;
 
 /// @brief Load a file from disk
 /// @return A vector of the bytes making up the requested file. This
 /// will be empty if the file is not found
-std::vector<char> load_file(const char* path);
+    std::vector<char> load_file(const char *path);
+} // namespace mt
