a10a1dd59922c64379d998b2d0f918317bfd962d
Clay <clay@clay66.dev>
2024-11-26 12:16:35 +0000

    Write `const char*` because it's better

--- src/backend.cpp
+++ src/backend.cpp
@@ -3,6 +3,7 @@
 #include <filesystem>
 #include <fstream>
 #include <libtorrent/read_resume_data.hpp>
+#include <libtorrent/write_resume_data.hpp>
 
 std::string storage_dir() noexcept {
     std::string root_dir;
@@ -17,6 +18,12 @@ std::string storage_dir() noexcept {
 }
 
 namespace fs = std::filesystem;
+
+std::vector<char> load_file(const char* filename) {
+    std::ifstream ifs(filename, std::ios_base::binary);
+    ifs.unsetf(std::ios_base::skipws);
+    return {std::istream_iterator<char>(ifs), std::istream_iterator<char>()};
+}
 
 std::vector<lt::add_torrent_params> resume_torrents() noexcept {
     std::string resume_dir = storage_dir() + "/resume-files";
--- src/backend.hpp
+++ src/backend.hpp
@@ -2,6 +2,7 @@
 
 #include <libtorrent/add_torrent_params.hpp>
 #include <string>
+#include <string_view>
 #include <vector>
 
 /// @brief Return the directory in which MicroTorrent will store all its data
@@ -14,3 +15,8 @@ std::string storage_dir() noexcept;
 /// @return A vector of the torrents which were resumed. This may be empty
 /// if no torrents were actually resumed
 std::vector<lt::add_torrent_params> resume_torrents() noexcept;
+
+/// @brief Load a file from disk
+/// @return A vector of the bytes making up the requested file. This
+/// will be empty if the file is not found
+std::vector<char> load_file(const char* path);
